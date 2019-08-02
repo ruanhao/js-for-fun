271c59e384e8d6a0a299230ab51d72010d78d5c5
Hao Ruan <ruanhao1116@gmail.com>
2019-08-02 22:49:43 +0800

    [React] - Enable html-webpack-plugin

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,3 +1,14 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    "template": path.join(__dirname, './src/index.html'),             // source file
+    "filename": "index.html",   // 内存中映射文件
+})
+
 module.exports = {
-    "mode": "development"
+    "mode": "development",
+    "plugins": [
+        htmlPlugin
+    ]
 }
