2d6e7dacd5bcd77fba71f744eb6367dd5fc7f110
Hao Ruan <ruanhao1116@gmail.com>
2019-08-05 22:06:26 +0800

    [React] - Send read-only props to Component

--- react/src/index.js
+++ react/src/index.js
@@ -12,8 +12,8 @@ let b = true;
 const h1 = <h1>H1</h1>;
 // React 中，需要把 key 添加给被 forEach, map 或 for 循环直接控制的元素
 const arr = [
-    <h2 id="0">H2</h2>,
-    <h3 id="1">H3</h3>,
+    <h2 key="0">H2</h2>,
+    <h3 key="1">H3</h3>,
 ];
 const strArr = [ 'a', 'b', 'c' ];
 // const myDiv = <div id='mydiv'>Test div</div>;
@@ -33,5 +33,22 @@ ReactDOM.render(<div>
                 <hr/>
                 {arr}
                 <hr/>
-                {strArr.map((str, index) => <h5 id={index}>{str}</h5>)}
+                {strArr.map((str, index) => <h5 key={index}>{str}</h5>)}
                 </div>, document.getElementById('app'));
+
+// 第一种创建组件的方式
+function Hello(props) {
+    return <div>Hello {props.name}, age: {props.age}, gender: {props.gender}</div>;
+}
+
+const user = {
+    "name": "Peter",
+    "age": 30,
+    "gender": "male"
+};
+
+ReactDOM.render(<div>
+                Component Test
+                <Hello {...user}></Hello>
+                <Hello name="Mary" age="20" gender="female"></Hello>
+                </div>, document.getElementById('component'));
