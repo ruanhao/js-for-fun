f59ae8ef34272477c1d51d77bf93ec1889272b8a
Hao Ruan <haoru@cisco.com>
2019-08-05 11:05:11 +0800

    [React] - Basic usage of rendering

--- react/src/index.js
+++ react/src/index.js
@@ -1,1 +1,11 @@
 console.log("hello world");
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create virtual dom
+// const myDom = React.createElement('h1', null, 'Test H1');
+const myDom = React.createElement('h2', {"id": "myh2", "title": "This is h2"}, 'Test H2');
+
+// Render virtual dom
+ReactDOM.render(myDom, document.getElementById('app'));
